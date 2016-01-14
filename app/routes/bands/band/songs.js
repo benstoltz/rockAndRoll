4711df0bbd59cc123916b9fefa6a65ec77f76fbb
Ben Stoltz <bstoltz@esri.com>
2016-01-14 11:53:28 -0500

    Star rating component
    Start rating component, user can add new bands/songs.

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
+import Song from '../../../models/song';
 
 export default Ember.Route.extend({
     model: function() {
         return this.modelFor('bands.band');
+    },
+
+    actions: {
+        createSong: function () {
+            let controller = this.get('controller');
+            let band = this.modelFor('bands.band');
+            let title = controller.get('title');
+
+            let song = Song.create({ title: title, band: band });
+            band.get('songs').pushObject(song);
+            controller.set('title', '');
+        }
     }
 });
